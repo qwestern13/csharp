383599cadd03ce244c63674638c8d78620607e22
Ilya Shaulov <ilya.shaulov@yandex.ru>
2024-09-20 18:48:35 +0500

    Добавьте файлы проекта.

---
+++ myfirstapp/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace myfirstapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello, C#!");
+
+            var sum1 = StaticSum(6, 7);
+            Console.WriteLine("sum1 = " + sum1);
+
+        }
+
+        public static int StaticSum(int a, int b)
+        {
+
+            var sum = a + b;
+
+            return sum;
+        }
+    }
+
+    public class MyFirstClass
+    {
+        public static void 
+    }
+}
