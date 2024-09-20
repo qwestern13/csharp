1d74c654ffc68c7c7947c0c4366e12096c7f7784
Ilya Shaulov <ilya.shaulov@yandex.ru>
2024-09-20 21:16:24 +0500

    ещё немного изменений

--- myfirstapp/Program.cs
+++ myfirstapp/Program.cs
@@ -4,7 +4,6 @@
     {
         static void Main(string[] args)
         {
-
             Console.WriteLine("Hello, C#!");
 
             var sum1 = StaticSum(6, 7);
@@ -13,15 +12,43 @@
             var test = MyFirstClass.MyFirstMethod(6, 9);
             Console.WriteLine(test);
 
+            var MyClass = new MyFirstClass();
+            var sum3 = MyClass.MySecondMethod(21, 7);
+            Console.WriteLine(sum3);
+
+            WriteLineCustomString("Void method");
+            WriteLineHelloCSharp();
+
+
+            var factorial = Fact(5);
+            Console.WriteLine("factorial = " + factorial);
         }
 
         public static int StaticSum(int a, int b)
         {
-
             var sum = a + b;
 
             return sum;
         }
+        
+        public static void WriteLineCustomString(string s)
+        {
+            Console.WriteLine(s);
+        }
+
+        public static void WriteLineHelloCSharp()
+        {
+            Console.WriteLine("Hello, CSharp!");
+        }
+
+        public static int Fact(int n)
+        {
+            if (n == 1)
+                return 1;
+
+            return n * Fact(n - 1);
+        }
+
     }
 
     public class MyFirstClass
@@ -30,10 +57,20 @@
         {
             Console.WriteLine("test new class and method");
 
+            var sum2 = a + b + 1;
+
+            return sum2;
+        }
+
+        public int MySecondMethod(int a, int b)
+        {
+            Console.WriteLine("this is the second class");
+
             var sum = a + b + 1;
 
             return sum;
         }
+
     }
 
 }
