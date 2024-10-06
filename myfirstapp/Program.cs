a39fba29df4b8145deb505711f43972cca1bd062
Ilya Shaulov <ilya.shaulov@yandex.ru>
2024-10-06 18:04:38 +0500

    иммутабельность

--- myfirstapp/Program.cs
+++ myfirstapp/Program.cs
@@ -20,7 +20,26 @@
             // interpolation
             var name = "John";
             var info = $"Hello, {name}!";
+            var busy = "How are you?";
             Console.WriteLine(info);
+            Console.WriteLine($"{info} I'm fine, 10x! And... {busy}");
+
+            // date
+            Console.WriteLine($"It's time to make a change: {DateTime.Now:dd'/'MM'/'yyyy}!");
+            Console.WriteLine($"It's time to make a new format: |{DateTime.Now,15:dd'/'MM'/'yyyy}|");
+
+            // imm
+            var plus = "Plus!";
+            // plus[1] = "error";
+            Console.WriteLine(plus[3]);
+
+            // inter
+            var s1 = "Hel";
+            var s2 = "Hel" + "lo!";
+            var s3 = "lo";
+            var s4 = $"Hel{s3}";
+            Console.WriteLine(s2 + "\n" + s4.Replace("Hel", "Al"));
+            Console.WriteLine(s2 == s4);
         }
     }
 }
