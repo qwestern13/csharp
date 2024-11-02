70081d480c74ffd84742e8988bc6bbd286d8b18d
Ilya Shaulov <shaulov@macroscop.com>
2024-11-02 18:34:55 +0500

    исправлена первая полоска

--- myfirstapp/Program.cs
+++ myfirstapp/Program.cs
@@ -4,8 +4,7 @@
     {
         public static void Main(string[] args)
         {
-          // Console.WriteLine("Hello World!");
-          WriteTable(6, "test");
+          WriteTable(3, "testtt");
           /*
           int inputNumber = int.Parse(Console.ReadLine());
           string? inputString = Console.ReadLine();
@@ -29,8 +28,8 @@
             }
 
             int wight = s.Length + 2 * (n - 1);
-            int heightCenter = (int)Math.Floor((double)((2 * n - 1) / 2));
-
+            //int heightCenter = (int)Math.Floor((double)((2 * n - 1) / 2));
+            //Console.WriteLine(heightCenter);
             if (wight > 40)
             {
                 return;
@@ -43,24 +42,24 @@
             Console.WriteLine();*/
             CrossLine(wight);
             
-            for (int i = 1; i < 2 * n - 1; i++)
+            for (int i = 1; i <= 2 * n - 1; i++)
             {
                 Console.Write('+');
-                if(i == heightCenter)
+                if(i == n)
                 {
-                    for (int j = 1; j <= n - 2; j++)
+                    for (int j = 1; j <= n - 1; j++)
                     {
                         Console.Write(' ');
                     }
                     Console.Write(s);
-                    for (int j = 1; j <= n - 2; j++)
+                    for (int j = 1; j <= n - 1; j++)
                     {
                         Console.Write(' ');
                     }
                 }
                 else
                 {
-                    for (int j = 1; j <= wight - 2; j++)
+                    for (int j = 1; j <= wight; j++)
                     {
                         Console.Write(' ');
                     }
@@ -73,13 +72,18 @@
         
         private static void CrossLine(int wight)
         {
-            for (int i = 1; i <= wight; i++)
+            for (int i = 1; i <= wight + 2; i++)
             {
                 Console.Write('+');
             }
             Console.WriteLine();
             
         }
+
+        /*public static void ChessDeck(int wight, int height)
+        {
+            while
+        }*/
 
     }
 
